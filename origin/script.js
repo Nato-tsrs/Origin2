a5e966741e0957abe4fb5b3aaa202d28d3c5470d
Nato Tsertsvadze <76736484+Nato-tsrs@users.noreply.github.com>
2022-04-10 23:53:58 +0400

    Add files via upload

---
+++ origin/script.js
@@ -0,0 +1,14 @@
+function menu(){
+    var menu=document.getElementById("left");
+    menu.style.visibility="visible";
+    menu.style.width="400px";
+    var clo=document.getElementById("close");
+    clo.style.display="block";
+
+}
+
+function exit(){
+    var menu=document.getElementById("left");
+    menu.style.width="0";
+   
+}
